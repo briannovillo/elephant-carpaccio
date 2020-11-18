51c977be13ad903d775eb50e253c05baf21c91cf
Brian Novillo <brian.novillo@mercadolibre.com>
2020-11-17 23:17:49 -0300

    Como usuario quiero ingresar como tercer argumento la abreviatura de un "estado" de los EEUU y que me muestre el porcentaje de impuesto aplicado a las compras en ese estado para saber si se cobra o no un porcentaje.

--- factura.rb
+++ factura.rb
@@ -1,3 +1,14 @@
+def getTaxForState(state)
+    @taxes = {
+        "UT" => 6.85,
+        "NV" => 8.00,
+        "TX" => 6.25,
+        "AL" => 4.00,
+        "CA" => 8.25
+    }
+    return @taxes[state]
+end
+
 def multiply(number1, number2)
     return number1.to_i * number2.to_i
 end
@@ -7,7 +18,7 @@ def renderRow1(quantity, unitPrice, baseAmount)
 end
 
 def renderRow2(state)
-    return "#{state}"
+    return "#{state} #{getTaxForState(state)}"
 end
 
 def main(quantity, unitPrice, state)
