f081b516bc9a7fb2cdfe574b8ccb44383e9538ff
Brian Novillo <brian.novillo@mercadolibre.com>
2020-11-17 23:35:48 -0300

    Como usuario quiero ver el monto total de impuestos aplicados en mi factura para entender si efectivamente se me cobró algo o no.

--- factura.rb
+++ factura.rb
@@ -9,6 +9,10 @@ def getTaxForState(state)
     return @taxes[state]
 end
 
+def percentage(number, percent)
+    return ((percent / 100) * number).round(2)
+end
+
 def multiply(number1, number2)
     return number1.to_i * number2.to_i
 end
@@ -17,14 +21,16 @@ def renderRow1(quantity, unitPrice, baseAmount)
     return "# #{quantity} * #{unitPrice} = #{baseAmount}"
 end
 
-def renderRow2(state)
-    return "#{state} #{getTaxForState(state)} = "
+def renderRow2(state, tax, percentage)
+    return "#{state} #{tax} = #{percentage}"
 end
 
 def main(quantity, unitPrice, state)
     @baseAmount = multiply(quantity, unitPrice)
+    @tax = getTaxForState(state)
+    @percentage = percentage(@baseAmount, @tax)
 
-    "#{renderRow1(quantity, unitPrice, @baseAmount)}\n#{renderRow2(state)}"
+    "#{renderRow1(quantity, unitPrice, @baseAmount)}\n#{renderRow2(state, @tax, @percentage)}"
 end
 
 puts main(ARGV[0], ARGV[1], ARGV[2])
