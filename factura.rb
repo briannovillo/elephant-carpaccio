a52b75c76a96fd86bf53d4f06010579b051199fb
Brian Novillo <brian.novillo@mercadolibre.com>
2020-11-17 21:31:18 -0300

    Como usuario quiero ver en pantalla el total entre la multiplicación del primer y segundo argumento para saber el monto base de mi factura.

--- factura.rb
+++ factura.rb
@@ -1,9 +1,15 @@
-def renderRow1(quantity, unitPrice)
-    return "# #{quantity} * #{unitPrice} = "
+def multiply(number1, number2)
+    return number1.to_i * number2.to_i
+end
+
+def renderRow1(quantity, unitPrice, baseAmount)
+    return "# #{quantity} * #{unitPrice} = #{baseAmount}"
 end
 
 def main(quantity, unitPrice)
-    renderRow1(quantity, unitPrice)
+    @baseAmount = multiply(quantity, unitPrice)
+
+    renderRow1(quantity, unitPrice, @baseAmount)
 end
 
 puts main(ARGV[0], ARGV[1])
